066ebc2add9deeb54ac71f492a88e81be4aba01e
StanislavGro <stanislav.skii2000@mail.ru>
2024-03-27 17:03:48 +0700

    add .env file

---
+++ src/test/java/ru/youngstanis/applicationlauncher/FluxAndMonoTest.java
@@ -0,0 +1,39 @@
+package ru.youngstanis.applicationlauncher;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoTest {
+
+    @Test
+    public void monoSubscriptionTest() {
+        Mono<String> stringMono = Mono
+                .just("Spring Boot")
+                .log();
+
+        stringMono.subscribe(System.out::println);
+    }
+
+    @Test
+    public void monoVerifyTest() {
+        Mono<String> stringMono = Mono.just("Spring");
+
+        StepVerifier.create(stringMono.log())
+                .expectNext("Spring")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void MonoErrorTest() {
+
+        Mono<Object> exceptionMono = Mono.error(new RuntimeException("My Exception!!!1!!"))
+                .log();
+
+        StepVerifier.create(exceptionMono)
+                .expectError(RuntimeException.class)
+                .verify();
+
+    }
+}
